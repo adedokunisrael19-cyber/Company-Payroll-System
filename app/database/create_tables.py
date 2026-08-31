24b5d1e31e756511b41817ae01b8e86ee1bc1167
Kehinde <116885712+Taofeek-Kehinde@users.noreply.github.com>
2026-08-31 08:33:22 +0100

    Delete app directory

--- app/database/create_tables.py
+++
@@ -1,7 +0,0 @@
-from app import Base, engine
-from app import Employee, Payroll
-
-
-Base.metadata.create_all(bind=engine)
-
-print("Tables created successfully!")
--- app/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from app import Employee
-from app import Payroll
